3d2cd0eb8d9dc5578e3d4ad57df58c2ae30066ff
Yamini Agarwal <yaminiagarwal0307@gmail.com>
2024-01-09 03:07:05 +0530

    changes

--- seeds/index.js
+++ seeds/index.js
@@ -3,7 +3,12 @@ const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+  }
+  const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp'
+
+mongoose.connect(dbUrl, {
     // useNewUrlParser: true,
     // useCreateIndex: true,
     // useUnifiedTopology: true
